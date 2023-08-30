36210cdf46cd793b5bf7db2a14a329e51598e06b
josphatgachure <56233475+josphatgachure@users.noreply.github.com>
2023-08-30 09:55:48 +0300

    Add files via upload

---
+++ Node/Users.js
@@ -0,0 +1,23 @@
+const users = [{
+    
+        id: 1,
+        name: "Josphasito",
+        email: "Josphasito@gmail.com"
+    },
+    {
+        id: 2,
+        name: "John",
+        email: "John@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Gushar",
+        email: "Gushar@gmail.com"
+    },
+
+    {
+        id: 4,
+        name: "Kellykush",
+        email: "Kellykush@gmail.com"
+    } ];
+module.exports = users;
