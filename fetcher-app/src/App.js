dac5c7b7718197415e9113cbec4486f970ba0561
josphatgachure <56233475+josphatgachure@users.noreply.github.com>
2023-08-30 09:57:27 +0300

    Add files via upload

---
+++ fetcher-app/src/App.js
@@ -0,0 +1,34 @@
+import React,{useEffect, useState} from 'react'
+
+
+const App =()=>{
+  const [users,setUsers] =useState([])
+
+const fetchUserData =()=>{
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+      return response.json()
+    })
+    .then(data =>{
+      setUsers(data)
+    })
+  }
+
+  useEffect(() => {
+    fetchUserData()
+  }, [])
+
+  return(
+    <div>
+      {users.length > 0 && (
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.address.street}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
+
+export default App;
